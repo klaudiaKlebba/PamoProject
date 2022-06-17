f7ca8f19fd870e22d98742fe9087379937c4f110
klaudiaKlebba <s18870@pjwstk.edu.pl>
2022-06-17 14:44:03 +0200

    part of Location MainActivity

--- app/src/main/java/com/example/pamoproject/MainActivity.kt
+++ app/src/main/java/com/example/pamoproject/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.example.pamoproject
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        supportActionBar?.hide()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        activityMainBinding.rlMainLayout.visibility= View.GONE
+
+        getCurrentLocation();
+    }
+    private fun getCurrentLocation(){
+        if(checkPermission())
+        {
+            if(isLocationEnabled())
+            {
+
+                if (ActivityCompat.checkSelfPermission(
+                        this,android.Manifest.permission.ACCESS_FINE_LOCATION
+                )!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    ){
+                    requestPermission()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) {
+                    val location: Location?=task.result
+                    if (location == null)
+                    {
+                        Toast.makeText(this,"Null Recieved",Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        //fetch weather here
+
+                        fetchCurrentLocationWeather(location.latitude.toString(),location.longitude.toString())
+
+                    }
+                }
+
+            }
+            else{
+                Toast.makeText(this,"Turn on location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else{
+            requestPermission()
+        }
+    }
+
+    private fun fetchCurrentLocationWeather(latitude: String, longitude: String) {
+
+    }
+
+    private fun isLocationEnabled():Boolean{
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    }
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        )
+    }
+    companion object{
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+    }
+
+    private fun chceckPermissions(): Boolean
+    {
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
+        ==PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
     }
 }
